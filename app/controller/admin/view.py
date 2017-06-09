dc1707cc119de176eaa35a90530a5820a2e78cb0
kerrygao <1509699669@qq.com>
2017-06-09 14:42:57 +0800

    ad

--- app/controller/admin/view.py
+++ app/controller/admin/view.py
@@ -3,10 +3,10 @@
 from app.controller.admin import admin
 from flask import render_template
 from flask_login import login_required
+
 @admin.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
-    print '11111111'
     return render_template('admin/dashboard.html')
 
 
